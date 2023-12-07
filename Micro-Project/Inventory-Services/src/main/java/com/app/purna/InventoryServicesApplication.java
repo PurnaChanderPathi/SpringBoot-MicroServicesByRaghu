59caf78bbd43c4baa5ff68fa7dda3fe6548789a4
PurnaChanderPathi <pathipurnachander@gmail.com>
2023-12-07 16:11:12 +0530

    SBMS

--- Micro-Project/Inventory-Services/src/main/java/com/app/purna/InventoryServicesApplication.java
+++ Micro-Project/Inventory-Services/src/main/java/com/app/purna/InventoryServicesApplication.java
@@ -1,8 +1,13 @@
 package com.app.purna;
 
+import com.app.purna.Entity.Inventory;
+import com.app.purna.dto.InventoryResponse;
+import com.app.purna.repository.InventoryRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -12,4 +17,20 @@ public class InventoryServicesApplication {
 		SpringApplication.run(InventoryServicesApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner loadData(InventoryRepository inventoryRepository)
+	{
+		return args -> {
+			Inventory inventory = new Inventory();
+			inventory.setSkuCode("iphone_13");
+			inventory.setQuantity(100);
+
+			Inventory inventory1 = new Inventory();
+			inventory1.setSkuCode("iphone_13_red");
+			inventory1.setQuantity(0);
+
+			//inventoryRepository.save(inventory);
+			//inventoryRepository.save(inventory1);
+		};
+	}
 }
