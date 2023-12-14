93ff5aace83be06b2ef8201908f414bebfabceed
PurnaChanderPathi <pathipurnachander@gmail.com>
2023-12-14 15:17:52 +0530

    Kafka

---
+++ Kafka/kafka-producer-example/src/main/java/com/app/purna/service/KafkaMessagePublisher.java
@@ -0,0 +1,30 @@
+package com.app.purna.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.CompletableFuture;
+
+@Service
+public class KafkaMessagePublisher {
+
+    @Autowired
+    private KafkaTemplate<String,Object> template;
+
+    public void sendMessageToTopic(String message)
+    {
+        CompletableFuture<SendResult<String, Object>> future = template.send("purna-demo4", message);
+        future.whenComplete((result,ex)->
+        {
+            if(ex == null)
+            {
+                System.out.println("Send message=[" + message +
+                        "] with offset=[" + result.getRecordMetadata().offset() + "]" );
+            }else {
+                System.out.println("Unable to send message=[" + message + "] due to : " +ex.getMessage());
+            }
+        });
+    }
+}
